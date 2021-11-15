d069eb48f7bebf164e43482591627ce29da5a96a
RocioM13 <89863054+RocioM13@users.noreply.github.com>
2021-11-15 19:46:50 -0300

    Add files via upload

---
+++ receta 7/javascript/añadir.js
@@ -0,0 +1,25 @@
+function aviso() {
+alert ("¿Esta dispuesto a ver el mejor sitio del mundo mundial?")
+}
+
+function adventencia() { alert ("MR Cook Diseñador Web Digital")
+}
+
+function cambiarfondo() { document.body.style.backgroundColor = "black";
+}
+
+function color() {
+  document.getElementById("myP").style.color = "pink";}
+
+function sombra() {document.getElementById("demo").style.textShadow = "5px 5px 1px #ff0000, 10px 10px 1px #0000ff";}
+
+function AmpliarImg(x) {
+  x.style.height ="20%";
+  x.style.width ="20%";
+
+}
+function AchicarImg(x) {
+  x.style.height ="5%";
+  x.style.width ="5%";
+
+}
