fc8c48a24a13ac9be773c4bb04615dc0f404755f
RocioM13 <89863054+RocioM13@users.noreply.github.com>
2021-11-23 23:47:13 -0300

    Add files via upload

---
+++ proyecto final/javascript/button.js
@@ -0,0 +1,5 @@
+function aviso() {
+alert ("¿Esta dispuesto a ver el mejor cine del mundo?")
+}
+function adventencia() { alert ("Te informamos que las funciones de Toccine estara cerrado en previo aviso")
+}
